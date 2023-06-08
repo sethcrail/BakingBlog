5b1d65055a96c687c93a467b232a9ae310c0ee12
sethcrail <sethcrail@gmail.com>
2023-06-07 19:20:22 -0700

    added start and end screen to game, created bad objects

--- public/js/play.js
+++ public/js/play.js
@@ -5,14 +5,20 @@ const ctx = canvas.getContext('2d');
 canvas.width = 360;
 canvas.height = 720;
 
+const canvasCenterX = canvas.width / 2;
+const canvasCenterY = canvas.height / 2;
+
+let score = 0;
+let gameOver = false;
+
 let foodArray = [];
-const numberOfFood = 2;
-let foodSpeedScale = 6;
+const numberOfFood = 4;
+let foodSpeedScale = 2;
 const foodSpeedMinimum = 2;
 const playerHeight = 50;
 const playerWidth = 100;
 
-let score = 0;
+ctx.font = "48px Crimson-Text serif";
 
 const canvasPosition = {
     x: canvasRect.x - window.scrollX,
@@ -29,62 +35,83 @@ onmousemove = (e) => {
     mouse.y = e.y;
 }
 
-ctx.font = "48px Crimson-Text serif";
 function drawScore() {
     ctx.fillText("Score: " + score, 10, 50);
 }
+
+//Game Entities
 
 class Player {
     constructor() {
         this.width = playerWidth;
         this.height = playerHeight;
-        this.x = (0.5 * canvas.width) - (0.5 * this.width);
+        this.x = canvasCenterX - this.width;
         this.y = canvas.height - this.height;
     }
     draw() {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     update() {
-        this.x = mouse.x - canvasPosition.x - (0.5 * this.width);
+        this.x = mouse.x - canvasPosition.x - (this.width / 2);
     }
 }
 
 class Food {
     constructor() {
         this.size = 50;
-        this.x = (0.5 * this.size) + Math.random() * (canvas.width - this.size);
+        this.x = (this.size / 2) + Math.random() * (canvas.width - this.size);
         this.y = 0 - this.size;
-        this.speed = foodSpeedMinimum + Math.random() * foodSpeedScale;
+        this.dy = foodSpeedMinimum + Math.random() * foodSpeedScale;
         this.angle = Math.random() * 360;
         this.spin = Math.random() < 0.5 ? -1 : 1;
+        this.type = Math.floor(Math.random() * 3);
     }
-    draw(){
+
+    draw() {
         ctx.save();
+        if (this.type == 0 || this.type == 1) {
+            ctx.fillStyle = "green";
+        } else {
+            ctx.fillStyle = "red";
+        }
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle * Math.PI/360 * this.spin);
         ctx.fillRect(0 - this.size/2, 0 - this.size/2, this.size, this.size);
         ctx.restore();
     }
     update() {
-        this.angle++;
         if (this.y >= canvas.height){
-            if (this.x <= player.x + player.width + 5 && this.x >= player.x - 5) {
-                score++;
-                console.log(score);
+
+            if (this.x <= player.x + player.width && this.x >= player.x) {
+                    if (this.type == 0 || this.type == 1) {
+                        score++;
+                    } else {
+                        gameOver = true;
+                    }
+
             }
+
             this.y = 0 - this.size;
-            this.x = (0.5 * this.size) + Math.random() * (canvas.width - this.size);
+            this.x = (this.size / 2) + Math.random() * (canvas.width - this.size);
+            this.dy = foodSpeedMinimum + Math.random() * foodSpeedScale;
+            this.type = Math.floor(Math.random() * 3);
+
         } else {
-            this.y += this.speed;
+            this.y += this.dy;
+            this.angle++;
         }
 
     }
 }
 
+//Game Architecture
+
 function init() {
     for (let i = 0; i < numberOfFood; i++) {
         foodArray.push(new Food());
     }
+    let score = 0;
+    let gameOver = false;
 }
 
 let player = new Player;
@@ -97,15 +124,34 @@ function animate() {
     }
 
     player.draw();
-    if (mouse.x > (canvasPosition.x + 0.5 * player.width) && mouse.x < (canvasRect.right - 0.5 * player.width)) {
+
+    if (mouse.x > (canvasPosition.x + player.width / 2) && mouse.x < (canvasRect.right - 0.5 * player.width)) {
         player.update();
     }
+
     drawScore();
-    requestAnimationFrame(animate);
+    
+    if (!gameOver) {
+        requestAnimationFrame(animate);
+    } else {
+        endGame();
+    }
+
 }
 
 function startGame() {
+    document.getElementById("start-btn").style.display = "none";
     init();
     animate();
-    document.getElementById('start-btn').style.display = "none";
+}
+
+function endGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillText("Game Over", canvasCenterX, canvasCenterY);
+    ctx.fillText("Final Score: " + score, canvasCenterX, canvasCenterY + 10);
+
+}
+
+function hiScorePrompt() {
+
 }
