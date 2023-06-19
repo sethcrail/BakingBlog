727f39595949a7dd2eeba963e8927094cfea6956
sethcrail <sethcrail@gmail.com>
2023-06-18 20:50:57 -0700

    Updated gallery modal, added modal close functionality changes

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -11,3 +11,15 @@ function closeModal() {
     modal.close();
 }
 
+modal.addEventListener("click", e => {
+    const modalDimensions = modal.getBoundingClientRect();
+    if (
+        e.clientX < modalDimensions.left ||
+        e.clientX > modalDimensions.right ||
+        e.clientY < modalDimensions.top ||
+        e.clientY > modalDimensions.bottom
+    ) {
+        modal.close();
+    }
+})
+
