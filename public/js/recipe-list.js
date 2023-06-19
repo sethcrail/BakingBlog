2eafc1acec6662de2e7defbc9078ce35e9771873
sethcrail <sethcrail@gmail.com>
2023-06-19 15:24:17 -0700

    consolidated ingredient list code, optimized for mobile

--- public/js/recipe-list.js
+++ public/js/recipe-list.js
@@ -13,20 +13,18 @@ function convertList() {
     document.querySelector(".imperial").classList.toggle("hidden");
 }
 
-const ingredientsList = document.getElementById('ingredient-list-container-mobile');
-let isOpen = false;
+const ingredientsList = document.getElementById('ingredient-list-container');
+let isOpen = true;
 console.log(ingredientsList.children[0].children[1]);
 
 function toggleIngredients() {
 
     if (isOpen) {
         ingredientsList.style.height = "2rem";
-        ingredientsList.style.width = "10rem";
         ingredientsList.children[0].children[1].style.transform = 'rotate(0deg)';
 
     } else {
         ingredientsList.style.height = "100%";
-        ingredientsList.style.width = "60%";
         ingredientsList.children[0].children[1].style.transform = 'rotate(180deg)';
 
     }
